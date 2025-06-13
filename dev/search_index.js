f2615df1f607e873234da657d45933465e30517d
Documenter.jl <documenter@juliadocs.github.io>
2025-06-13 23:20:21 +0000

    build based on 1f5f79a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SuiteSplinesExampleDep","page":"Home","title":"SuiteSplinesExampleDep","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SuiteSplinesExampleDep.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#SuiteSplinesExampleDep.testdep-Tuple{Any}","page":"Home","title":"SuiteSplinesExampleDep.testdep","text":"testme(x)\n\nSome function to test.\n\n\n\n\n\n","category":"method"}]
+}
